4a28eadb9c5a532e666582014c217ffe86cd9a0f
Mostafa Hassan <mostafahassan8799@gmail.com>
2024-03-16 23:40:59 -0700

    Add fetchFeaturedBooks implementation in HomeRemoteDataSourceImpl
    Implement the fetchFeaturedBooks method in HomeRemoteDataSourceImpl to fetch
    featured books using the ApiService and convert the data to BookEntity objects.

--- lib/Features/home/data/data_sources/remote/home_remote_data_source.dart
+++ lib/Features/home/data/data_sources/remote/home_remote_data_source.dart
@@ -1,15 +1,31 @@
+import 'package:bookly/Features/home/data/models/books_model/books_model.dart';
+import 'package:bookly/core/utils/api_service.dart';
+
 import '../../../domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
   Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>>  fetchNewestBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
 }
 
 class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+  HomeRemoteDataSourceImpl({required this.apiService});
   @override
-  Future<List<BookEntity>> fetchFeaturedBooks() {
+  Future<List<BookEntity>> fetchFeaturedBooks() async {
     // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+    var data = await apiService.get(
+        endPoint: 'volumes?Filtering=free-ebooks&q=prrogamming');
+    List<BookEntity> books = getBookList(data);
+    return books;
+  }
+
+  List<BookEntity> getBookList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    for (var item in data['items']) {
+      books.add(BooksModel.fromJson(item));
+    }
+    return books;
   }
 
   @override
