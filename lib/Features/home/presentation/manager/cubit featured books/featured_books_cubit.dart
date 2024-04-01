41d593155d0a2d401090947aa134b8ea8f2db4f4
Mostafa Hassan <mostafahassan8799@gmail.com>
2024-04-01 09:42:22 -0700

    /adding cubit✔

---
+++ lib/Features/home/presentation/manager/cubit featured books/featured_books_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:meta/meta.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+  }
+}
---
+++ lib/Features/home/presentation/manager/cubit featured books/featured_books_state.dart
@@ -0,0 +1,19 @@
+part of 'featured_books_cubit.dart';
+
+@immutable
+abstract class FeaturedBooksState {}
+
+class FeaturedBooksInitial extends FeaturedBooksState {}
+
+class FeaturedBooksLoading extends FeaturedBooksState {}
+
+class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errorMessage;
+  FeaturedBooksFailure({required this.errorMessage});
+}
+
+class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookEntity> books;
+
+  FeaturedBooksSuccess({required this.books});
+}
